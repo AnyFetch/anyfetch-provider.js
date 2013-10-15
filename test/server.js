edb46db582a979e7616cfac1cdda33c7f013daca
Neamar <neamar@neamar.fr>
2013-10-15 10:34:28 +0200

    Init callback first draft

---
+++ test/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var async = require('async');
+
+var accessGrant = "dqzknr54dzgd6f5";
+
+var initAccount = function(req, res, next) {
+  var preDatas = {
+    accessGrant: accessGrant
+  };
+  next(null, preDatas);
+};
+
+var connectAccountRetrieveTempToken = function(req, res, TempToken, next) {
+  // Retrieve temp token
+  TempToken.findOne({'datas.accessGrant': accessGrant}, cb);
+};
+
+var connectAccountRetrieveAuthDatas = function(req, res, preDatas, next) {
+  var datas = preDatas.accessGrant + "_accessToken";
+  next();
+};

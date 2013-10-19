cda5ba9de2594c30824323ecc7c90c7424ce465c
Neamar <neamar@neamar.fr>
2013-10-19 17:32:53 +0200

    .createTestFrontServer() function

--- test/server.js
+++ test/server.js
@@ -135,15 +135,7 @@ describe("ProviderServer.createServer()", function() {
       process.env.CLUESTR_FRONT = 'http://localhost:1337';
 
       // Create a fake HTTP server
-      var frontServer = restify.createServer();
-      frontServer.use(restify.acceptParser(frontServer.acceptable));
-      frontServer.use(restify.queryParser());
-      frontServer.use(restify.bodyParser());
-
-      frontServer.post('/oauth/token', function(req, res, next) {
-        res.send({access_token: "fake_access_token"});
-        next();
-      });
+      var frontServer = ProviderServer.createTestFrontServer();
       frontServer.listen(1337);
 
       var originalPreDatas = {
